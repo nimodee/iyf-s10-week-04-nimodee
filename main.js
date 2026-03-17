f3a2d625c28f40cadc0af51ab346a728bfbc24ea
DaisyWairimu <daisywairimu15.gmail.com>
2026-03-17 18:53:32 +0300

    Task 7.1: Javascript setup and variables

---
+++ main.js
@@ -0,0 +1,29 @@
+// Variable declarations
+let name = "Daisy";
+let age = 23;
+const birthYear = 2003;
+
+// typeof operator
+console.log(typeof name);    // string
+console.log(typeof age);     // number
+console.log(typeof true);    // boolean
+
+// let vs const
+let score = 100;
+score = 150;  // Works!
+
+const PI = 3.14159;
+// PI = 3;  // Error! const cannot be reassigned
+
+
+let myName = "Daisy";
+let myAge = 22;
+let isStudent = true;
+let favoriteColors = ["nude", "black", "white"];
+let todayDate = new Date();
+
+console.log("My name is:", myName);
+console.log("My age is:", myAge);
+console.log("Am I a student?", isStudent);
+console.log("My favorite colors are:", favoriteColors);
+console.log("Today's date is:", todayDate);
