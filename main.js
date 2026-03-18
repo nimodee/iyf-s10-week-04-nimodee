2f4bc2bbcf834401d55715ce7aadc18b442ed74b
DaisyWairimu <daisywairimu15.gmail.com>
2026-03-18 13:23:29 +0300

    Added index and main js

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
 // Variable declarations
 let name = "Daisy";
-let age = 23;
+let age = 22;
 const birthYear = 2003;
 
 // typeof operator
-console.log(typeof name);    // string
-console.log(typeof age);     // number
-console.log(typeof true);    // boolean
+console.log(typeof name);    
+console.log(typeof age);     
+console.log(typeof true);    
 
 // let vs const
 let score = 100;
-score = 150;  // Works!
+score = 150;
 
 const PI = 3.14159;
-// PI = 3;  // Error! const cannot be reassigned
 
-
+// Exercise variables
 let myName = "Daisy";
 let myAge = 22;
 let isStudent = true;
 let favoriteColors = ["nude", "black", "white"];
 let todayDate = new Date();
 
+// Console logs
 console.log("My name is:", myName);
 console.log("My age is:", myAge);
 console.log("Am I a student?", isStudent);
-console.log("My favorite colors are:", favoriteColors);
-console.log("Today's date is:", todayDate);
+console.log("My favorite colors:", favoriteColors);
+console.log("Today's date:", todayDate);
+
